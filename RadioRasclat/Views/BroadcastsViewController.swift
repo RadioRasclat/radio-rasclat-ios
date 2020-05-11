efd80aad920f9947ca2593f42aff10dbbcff6a4f
Domenik Töfflinger <domenikt96@yahoo.de>
2020-05-11 20:52:16 +0200

    BroadcastsViewController

---
+++ RadioRasclat/Views/BroadcastsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BroadcastsViewController.swift
+//  RadioRasclat
+//
+//  Created by Domenik Töfflinger on 11.05.20.
+//  Copyright © 2020 Domenik Toefflinger. All rights reserved.
+//
+
+import UIKit
+
+class BroadcastsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
