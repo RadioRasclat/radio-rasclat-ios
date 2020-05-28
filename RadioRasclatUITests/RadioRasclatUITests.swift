f41e7f2fe94bcea7ea2d67a85483e27e75511ad8
Domenik Töfflinger <domenikt96@yahoo.de>
2020-05-28 23:32:06 +0200

    rename RadioRasclatUITests

--- RadioRasclatUITests/RadioRasclatUITests.swift
+++ RadioRasclatUITests/RadioRasclatUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class Radio_RasclatUITests: XCTestCase {
+class RadioRasclatUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
