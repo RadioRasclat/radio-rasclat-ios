9e2580cf0b68952325d3888efd7771a083a54126
Domenik Töfflinger <domenikt96@yahoo.de>
2020-06-04 15:55:45 +0200

    Update ScreenshotsUITests.swift

--- ScreenshotsUITests/ScreenshotsUITests.swift
+++ ScreenshotsUITests/ScreenshotsUITests.swift
@@ -70,4 +70,21 @@ class ScreenshotsUITests: XCTestCase {
         app.tables/*@START_MENU_TOKEN@*/.staticTexts["Radio Rasclat presents: Fruity Loup • Vol.2 (24.04.2020)"]/*[[".cells.staticTexts[\"Radio Rasclat presents: Fruity Loup • Vol.2 (24.04.2020)\"]",".staticTexts[\"Radio Rasclat presents: Fruity Loup • Vol.2 (24.04.2020)\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         snapshot("2BroadcastsDetail")
     }
+    
+    func testScheduleScreenshots() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let systemAlerts = XCUIApplication(bundleIdentifier: "com.apple.springboard").alerts
+        if systemAlerts.buttons["Allow"].exists {
+            systemAlerts.buttons["Allow"].tap()
+        }
+        
+        app.tabBars.buttons["Schedule"].tap()
+        snapshot("3Schedule")
+    }
 }
