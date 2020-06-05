c634ab2e1536b3a483ba4179814e26dc62b77a79
Domenik Töfflinger <domenikt96@yahoo.de>
2020-06-05 03:41:09 +0200

    Update ScheduleViewController.swift

--- RadioRasclat/Views/Schedule/ScheduleViewController.swift
+++ RadioRasclat/Views/Schedule/ScheduleViewController.swift
@@ -13,6 +13,11 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
     
     @IBOutlet weak var mondayTableView: UITableView!
     @IBOutlet weak var tuesdayTableView: UITableView!
+    @IBOutlet weak var wednesdayTableView: UITableView!
+    @IBOutlet weak var thursdayTableView: UITableView!
+    @IBOutlet weak var fridayTableView: UITableView!
+    @IBOutlet weak var saturdayTableView: UITableView!
+    @IBOutlet weak var sundayTableView: UITableView!
     
     var schedule: Schedule?
     
@@ -21,11 +26,31 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
 
         mondayTableView.delegate = self
         mondayTableView.dataSource = self
-        mondayTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        mondayTableView.register(UITableViewCell.self, forCellReuseIdentifier: "mondayCells")
         
         tuesdayTableView.delegate = self
         tuesdayTableView.dataSource = self
-        tuesdayTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell1")
+        tuesdayTableView.register(UITableViewCell.self, forCellReuseIdentifier: "tuesdayCells")
+        
+        wednesdayTableView.delegate = self
+        wednesdayTableView.dataSource = self
+        wednesdayTableView.register(UITableViewCell.self, forCellReuseIdentifier: "wednesdayCells")
+        
+        thursdayTableView.delegate = self
+        thursdayTableView.dataSource = self
+        thursdayTableView.register(UITableViewCell.self, forCellReuseIdentifier: "thursdayCells")
+        
+        fridayTableView.delegate = self
+        fridayTableView.dataSource = self
+        fridayTableView.register(UITableViewCell.self, forCellReuseIdentifier: "fridayCells")
+        
+        saturdayTableView.delegate = self
+        saturdayTableView.dataSource = self
+        saturdayTableView.register(UITableViewCell.self, forCellReuseIdentifier: "saturdayCells")
+        
+        sundayTableView.delegate = self
+        sundayTableView.dataSource = self
+        sundayTableView.register(UITableViewCell.self, forCellReuseIdentifier: "sundayCells")
         
         downloadSchedule {
             self.mondayTableView.reloadData()
@@ -48,6 +73,26 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
         if tableView == self.tuesdayTableView {
             count = schedule.tuesday.count
         }
+        
+        if tableView == self.wednesdayTableView {
+            count = schedule.wednesday.count
+        }
+        
+        if tableView == self.thursdayTableView {
+            count = schedule.thursday.count
+        }
+        
+        if tableView == self.fridayTableView {
+            count = schedule.friday.count
+        }
+        
+        if tableView == self.saturdayTableView {
+            count = schedule.saturday.count
+        }
+        
+        if tableView == self.sundayTableView {
+            count = schedule.sunday.count
+        }
 
         return count
 
@@ -58,19 +103,45 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
         var cell:UITableViewCell?
         
         if tableView == self.mondayTableView {
-            cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath)
+            cell = tableView.dequeueReusableCell(withIdentifier: "mondayCells", for: indexPath as IndexPath)
             let mondayScheduleDetail = schedule?.monday[indexPath.row]
             cell!.textLabel!.text = mondayScheduleDetail?.name
-            cell!.imageView!.image = UIImage(contentsOfFile: "placeholderImage")
-
         }
 
         if tableView == self.tuesdayTableView {
-            cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath as IndexPath)
+            cell = tableView.dequeueReusableCell(withIdentifier: "tuesdayCells", for: indexPath as IndexPath)
             let tuesdayScheduleDetail = schedule?.tuesday[indexPath.row]
             cell!.textLabel!.text = tuesdayScheduleDetail?.name
-            cell!.imageView!.image = UIImage(contentsOfFile: "placeholderImage")
-
+        }
+        
+        if tableView == self.wednesdayTableView {
+            cell = tableView.dequeueReusableCell(withIdentifier: "wednesdayCells", for: indexPath as IndexPath)
+            let wednesdayScheduleDetail = schedule?.wednesday[indexPath.row]
+            cell!.textLabel!.text = wednesdayScheduleDetail?.name
+        }
+        
+        if tableView == self.thursdayTableView {
+            cell = tableView.dequeueReusableCell(withIdentifier: "thursdayCells", for: indexPath as IndexPath)
+            let thursdayScheduleDetail = schedule?.thursday[indexPath.row]
+            cell!.textLabel!.text = thursdayScheduleDetail?.name
+        }
+        
+        if tableView == self.fridayTableView {
+            cell = tableView.dequeueReusableCell(withIdentifier: "fridayCells", for: indexPath as IndexPath)
+            let fridayScheduleDetail = schedule?.friday[indexPath.row]
+            cell!.textLabel!.text = fridayScheduleDetail?.name
+        }
+        
+        if tableView == self.saturdayTableView {
+            cell = tableView.dequeueReusableCell(withIdentifier: "saturdayCells", for: indexPath as IndexPath)
+            let saturdayScheduleDetail = schedule?.saturday[indexPath.row]
+            cell!.textLabel!.text = saturdayScheduleDetail?.name
+        }
+        
+        if tableView == self.sundayTableView {
+            cell = tableView.dequeueReusableCell(withIdentifier: "sundayCells", for: indexPath as IndexPath)
+            let sundayScheduleDetail = schedule?.sunday[indexPath.row]
+            cell!.textLabel!.text = sundayScheduleDetail?.name
         }
 
         return cell!
