fec9b8216f20a4c12b702366e2144f1f4e1880f6
Domenik Töfflinger <domenikt96@yahoo.de>
2020-12-04 03:34:28 +0100

    Update RecordingDetailView.swift

--- RadioRasclat/Views/RecordingDetailView.swift
+++ RadioRasclat/Views/RecordingDetailView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct RecordingDetailView: View {
+    
+    @ObservedObject private var recordingDetailVM = RecordingDetailViewModel()
+    
+    var recording: Recording
+        
+    init(recording: Recording) {
+        self.recording = recording
+        recordingDetailVM.getMovieDetails(id: recording.id)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView(showsIndicators: false) {
+            
+            VStack(alignment: .leading) {
+                
+                RecordingDetailHeroImage(recording: recording)
+                
+                if recordingDetailVM.fetchedRecording?.genres.count ?? 0 >= 1 {
+                    
+                    // Genre
+                    Text("\(recordingDetailVM.fetchedRecording?.genres.first?.title ?? "")")
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                        .padding(2)
+                        .border(Color.gray)
+                    
+                }
+
+                // Synopsis
+                Text(recording.recordingDescription)
+                    .font(.body)
+                    .fixedSize(horizontal: false, vertical: true)
+                
+                if recordingDetailVM.fetchedRecording?.artists != nil {
+                    ArtistsView(artist: (recordingDetailVM.fetchedRecording?.artists)!)
+                        .buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
+        .padding()
+        .navigationBarTitle(recording.title)
     }
 }
 
 struct RecordingDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        RecordingDetailView()
+        RecordingDetailView(recording: Recording.example)
     }
 }
