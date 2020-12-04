1a0c73bfdba49ce628e2acef1d1d897f2e4ddcd2
Domenik Töfflinger <domenikt96@yahoo.de>
2020-12-04 03:33:29 +0100

    getRecordingsDetails in web service

--- RadioRasclat/Services/WebService.swift
+++ RadioRasclat/Services/WebService.swift
@@ -49,4 +49,45 @@ class WebService {
         // execute the HTTP request
         task.resume()
     }
+    
+    func getRecordingsDetails(recording: String, completion: @escaping (Recording?) -> ()) {
+            
+        guard let url = URL(string: "https://api.radio-rasclat.com/recordings/recording/\(recording)") else {
+            fatalError("Invalid URL")
+        }
+        
+        let config = URLSessionConfiguration.default
+        config.urlCache = URLCache.shared
+        let session = URLSession(configuration: config)
+        
+        let urlRequest = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 15.0)
+        let task = session.dataTask(with: urlRequest) { data, response, error in
+            
+            // Check for errors
+            guard error == nil else {
+                print ("error: \(error!)")
+                return
+            }
+            // Check that data has been returned
+            guard let data = data else {
+                print("No data")
+                return
+            }
+            
+            do {
+                
+                let decoder = JSONDecoder()
+                let recordingDetails = try decoder.decode(Recording.self, from: data)
+                
+                DispatchQueue.main.async {
+                    completion(recordingDetails)
+                }
+                
+            } catch let err {
+                print("Err", err)
+            }
+        }
+        // execute the HTTP request
+        task.resume()
+    }
 }
