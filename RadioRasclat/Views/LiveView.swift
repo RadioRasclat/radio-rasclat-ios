75daacf998d6d258bc59bd06e4376520875054f4
Domenik Töfflinger <domenikt96@yahoo.de>
2020-12-05 20:00:34 +0100

    Create LiveView.swift

---
+++ RadioRasclat/Views/LiveView.swift
@@ -0,0 +1,20 @@
+//
+//  LiveView.swift
+//  RadioRasclat
+//
+//  Created by Domenik Töfflinger on 05.12.20.
+//
+
+import SwiftUI
+
+struct LiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LiveView_Previews: PreviewProvider {
+    static var previews: some View {
+        LiveView()
+    }
+}
